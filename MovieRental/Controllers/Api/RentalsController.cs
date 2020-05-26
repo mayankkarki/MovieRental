2256f34dc76cd3a849d26d12d4d02756190b59b4
Mayank karki <mayank.karki90@hotmail.com>
2020-05-26 21:50:40 +0530

    aded new rental form and controllers

---
+++ MovieRental/Controllers/Api/RentalsController.cs
@@ -0,0 +1,52 @@
+﻿using MovieRental.Dtos;
+using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieRental.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private MovieRentalDbContext _dbContext;
+
+        public RentalsController()
+        {
+            _dbContext = new MovieRentalDbContext();
+        }
+
+        //api/rental
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDto rentalDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = _dbContext.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            var movies = _dbContext.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    CustomerId = rentalDto.CustomerId,
+                    DateRented = DateTime.Now,
+                    MovieId = movie.Id
+                };
+
+                _dbContext.Rentals.Add(rental);
+            }
+
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
