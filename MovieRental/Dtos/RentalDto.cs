1eb1c04cb6f87a88c505533a594b8d2adf25db3f
Mayank karki <mayank.karki90@hotmail.com>
2020-05-26 21:49:26 +0530

    added rental dto

---
+++ MovieRental/Dtos/RentalDto.cs
@@ -0,0 +1,18 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRental.Dtos
+{
+    public class RentalDto
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public List<int> MovieIds { get; set; }
+    }
+}
